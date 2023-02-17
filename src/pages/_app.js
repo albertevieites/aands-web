b8246ecd8f887502aaff9a871b59c2201645bdcd
Alberte Vieites <albertevieites@gmail.com>
2023-02-17 15:16:07 +0000

    feat:

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import Layout from '../components/layout/Layout';
 import '../styles/styles.scss';
 
@@ -5,6 +6,7 @@ export default function App({ Component, pageProps }) {
 	return (
 		<Layout>
 			<Component {...pageProps} />
+			<Analytics />
 		</Layout>
 	);
 }
