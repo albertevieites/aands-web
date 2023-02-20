5abb1e8b18ece73d399f03c5b1a69a9bd81319a0
Alberte Vieites <albertevieites@gmail.com>
2023-02-20 19:11:21 +0000

    style: :lipstick: lines in all pages

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import ProductDetails from 'src/components/cards/ProductDetails';
+import Line from 'src/components/tokens/Line';
+import Separator from 'src/components/tokens/Separator';
 
 import ProductCard from '../../components/cards/ProductCard';
 
@@ -7,20 +9,24 @@ export default function Products() {
 	const [selectedId, setSelectedId] = useState('');
 
 	return (
-		<div className='products'>
-			<ProductCard selectedId={selectedId} setSelectedId={setSelectedId} />
+		<>
+			<Separator />
+			<div className='products'>
+				<ProductCard selectedId={selectedId} setSelectedId={setSelectedId} />
 
-			{/* <ProductDetails /> */}
-			<>
-				{selectedId && (
-					<div
-						onClick={() => setSelectedId(null)}
-						className='layout-mini-modal-backdrop'
-					>
-					<ProductDetails />
-					</div>
-				)}
-			</>
-		</div>
+				{/* <ProductDetails /> */}
+				<>
+					{selectedId && (
+						<div
+							onClick={() => setSelectedId(null)}
+							className='layout-mini-modal-backdrop'
+						>
+							<ProductDetails />
+						</div>
+					)}
+				</>
+			</div>
+			<Line />
+		</>
 	);
 }
