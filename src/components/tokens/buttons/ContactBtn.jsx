8a9e9faa841e3204735c6e68ac68b26f5d65275f
Alberte Vieites <albertevieites@gmail.com>
2023-03-14 21:06:28 +0000

    add hamburger

--- src/components/tokens/buttons/ContactBtn.jsx
+++ src/components/tokens/buttons/ContactBtn.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ContactBtn() {
   return (
     <Link className='contact__btn' href='/contact'>
-      <span>Contact Us</span>
+      Contact Us
     </Link>
   )
 }
